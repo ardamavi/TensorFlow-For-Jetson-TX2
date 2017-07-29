8ed17feb2272782b1009dcce11e7b58b17cd932b
Arda Mavi <ardamavi2@gmail.com>
2017-07-29 15:08:43 +0300

    Files added

---
+++ install_tensorflow_tx1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Python 3.5 TensorFlow installing script for NVIDIA Jetson TX2 (Ubuntu)
+
+sudo apt-get install python3-pip
+
+pip3 install tensorflow-1.0.1-cp35-cp35m-linux_aarch64.whl
+
